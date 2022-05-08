77900874cfbfbb7ed97f359e2fcc6c8aa8fcb129
Fakepng <krit0kasemtewin@gmail.com>
2022-05-08 21:32:32 +0700

    Day 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script>
+	let todos = [];
+	let task = "";
+
+	const addTodo = () => {
+		let todo = {
+			task: task,
+			isComplete: false,
+			createdAt: new Date(),
+		};
+		todos = [todo, ...todos];
+		task = "";
+	};
+</script>
+
+<input type="text" placeholder="Add a task" bind:value={task} />
+<button on:click={addTodo}>Add</button>
+
+<ol>
+	{#each todos as todo}
+		<li class:complete={todo.isComplete}>{todo.task}</li>
+	{/each}
+</ol>
+
+<style>
+	.complete {
+		text-decoration: line-through;
+	}
+</style>
